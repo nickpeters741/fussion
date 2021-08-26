6e9bcd571b800b47a93870099e47a6dc27f02859
nickpeters741 <npeters741@gmail.com>
2021-08-26 10:36:59 -0700

    C code to write to stderr without using printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-
-int write(int filedes, const char *buf, unsigned int nbyte);
+#include <string.h>
+#include <unistd.h>
 /**
- * main - Entry point
- * @argc: number of arguments to main
- * @argv: pointer to pointer containing addresses of the arguments
- *
+ * main - main block
  * Return: 1
  */
-int main(int argc, char **argv)
+int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      64);
+	int s;
 
+	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      s);
 	return (1);
 }
