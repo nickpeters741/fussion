668b66f8b41f9dd35c6113a8c588638ea418f4b8
nickpeters741 <npeters741@gmail.com>
2021-08-26 10:30:59 -0700

    C code to write to stderr without using printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+fn main()
+{
+	eprintln!("and that piece of art if useful\" - Dora Korpar, 2015-10-19\n");
+
+	return(1);
+}
