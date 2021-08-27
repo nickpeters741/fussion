47bed0f58ecbf4872631cd1bc44a03c240fdfeaa
nickpeters741 <npeters741@gmail.com>
2021-08-27 13:29:26 -0700

    print alphabet in lower and upper case using putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+	char C = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+
+	return (0);
+}
