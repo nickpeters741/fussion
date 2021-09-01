8e9f14eac3dcaede51a23a009e55c10ed6c503c0
nickpeters741 <npeters741@gmail.com>
2021-09-01 10:38:16 -0700

    function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,70 +1,51 @@
 #include "main.h"
 
 /**
- * print_times_table - function that prints times table for a given number
- *@n: the given number to create the times table
-*/
+ * print_times_table - prints times table
+ * @n : times table to use
+ * Return:void
+ */
+
 void print_times_table(int n)
 {
-	int a = 0, b = 0;
 
-	if (n >= 0 && n < 16)
+int a = 0, rep, b;
+
+if (n < 0 || n > 15)
+	return;
+
+while (a <= n)
+{
+	for (b = 0; b <= n; b++)
 	{
-		while (a <= n)
+		rep = a * b;
+		if (b == 0)
+			_putchar('0' + rep);
+		else if (rep < 10)
 		{
-			b = 0;
-			while (b <= n)
-			{
-				if (a * b < 10)
-					comma_spaces_nums(1, a, b);
-				else if (a * b > 9 && a * b < 100)
-					comma_spaces_nums(2, a, b);
-				else
-					comma_spaces_nums(3, a, b);
-				b++;
-			}
-			_putchar('\n');
-			a++;
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('0' + rep);
+		}
+		else if (rep < 100)
+		{
+			_putchar(' ');
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
+		else
+		{
+			_putchar('0' + rep / 100);
+			_putchar('0' + (rep - 100) / 10);
+			_putchar('0' + rep % 10);
+		}
+		if (b < n)
+		{
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
+	_putchar('\n');
+	a++;
 }
-
-/**
- * comma_spaces_nums - prints commas, spaces and digits accordingly
- * @dig: digit value
- * @a: first multiplier
- * @b: second multiplier
-*/
-void comma_spaces_nums(int dig, int a, int b)
-{
-	if (b != 0)
-	{
-		int i;
-
-		_putchar(',');
-		if (dig == 1)
-			for (i = 0; i < 3; i++)
-				_putchar(' ');
-		else if (dig == 2)
-			for (i = 0; i < 2; i++)
-				_putchar(' ');
-		else
-			_putchar(' ');
-	}
-	if (dig == 1)
-	{
-		_putchar('0' + a * b);
-	}
-	else if (dig == 2)
-	{
-		_putchar('0' + ((a * b) / 10));
-		_putchar('0' + ((a * b) % 10));
-	}
-	else
-	{
-		_putchar('0' + ((a * b) / 100));
-		_putchar('0' + ((a * b) / 10) % 10);
-		_putchar('0' + ((a * b) % 10));
-	}
 }
-
