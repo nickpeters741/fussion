4c3ef7ec2e6c7ad5b90f5d8fb88ba49a9414a24a
nickpeters741 <npeters741@gmail.com>
2021-09-15 01:22:24 -0700

    Add header file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_sqrt(int n, int square);
+int is_prime_number(int n);
+int is_prime(int n, int i);
+int is_palindrome(char *s);
+int palindrome_check(char *s, int len, int i);
+int wildcmp(char *s1, char *s2);
+
+#endif /* MAIN_H */
