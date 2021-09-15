cea1e7e2a550635a988b230000a840c8aa4e62b2
nickpeters741 <npeters741@gmail.com>
2021-09-15 02:54:23 -0700

    function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,24 +1,51 @@
 #include "main.h"
 
 /**
- * wildcmp - returns 1 if strings match and 0 if not
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
  *
- * @s1: string 1
- * @s2: string 2
+ * @s: string
  * Return: int
  */
-int wildcmp(char *s1, char *s2)
+int is_palindrome(char *s)
 {
-	if (*s1 == *s2 && *s1 && *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	if (!*s1 && !*s2)
+	int len = _strlen_recursion(s), i = 0;
+
+	if (len == 0)
 		return (1);
-	if (*s2 == '*')
-	{
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-			return (1);
-		if (*(s1 + 1) && !*s2)
-			return (0);
-	}
-	return (0);
+	return (palindrome_check(s, len - 1, i));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: string
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	if (*s == '\0')
+		return (0);
+	len = _strlen_recursion(s + 1);
+	return (len + 1);
+}
+
+/**
+ * palindrome_check - returns 1 if palindrome and 0 if not
+ *
+ * @s: string
+ * @len: length of string
+ * @i: counter variable
+ * Return: int
+ */
+int palindrome_check(char *s, int len, int i)
+{
+	if (len == i)
+		return (1);
+	else if (s[len] != s[i])
+		return (0);
+	if (i < len)
+		return (palindrome_check(s, --len, ++i));
+	return (1);
 }
