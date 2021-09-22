d1d0d71f19bd8e5ade2570b7f32a55e2dbed34e6
nickpeters741 <npeters741@gmail.com>
2021-09-21 22:12:10 -0700

    updated header file

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+void *malloc(size_t size);
+void free(void *ptr);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+
+#endif /* MAIN_H */
