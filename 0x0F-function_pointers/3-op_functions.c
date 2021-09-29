6aa541a4256a972d8dfae621cb1441ae695f2b9b
nickpeters741 <npeters741@gmail.com>
2021-09-29 02:08:17 -0700

    function to choose operation to carry out based on operator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct function to perform
+ * the operation asked by the user
+ *
+ * @s: operator string
+ * Return: int
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (*s == *ops[i].op)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
