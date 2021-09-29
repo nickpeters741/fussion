b687bf310ddec84970420a43c2f7edc734ee89fc
nickpeters741 <npeters741@gmail.com>
2021-09-29 02:17:00 -0700

    function for basic operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: integer
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: integer
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: integer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: integer
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the modulus of two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: integer
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
